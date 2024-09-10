8c3e3085c9a3f00890bce3698ca583adcdf8ce09
MartinPettersen <searchingtheskyfor@gmail.com>
2024-09-10 14:10:01 +0200

    add text on image

--- app/components/(images)/ProjectImageCard.tsx
+++ app/components/(images)/ProjectImageCard.tsx
@@ -1,29 +1,33 @@
-import React from 'react'
-import Image from "next/image";
-
+import React from 'react';
+import Image from 'next/image';
 
 type Props = {
-    imageUrl: string;
-    textDescription: string;
-    layout: string
-}
+  imageUrl: string;
+  textDescription: string;
+  layout: string;
+};
 
-const ProjectImageCard = ( {imageUrl, textDescription, layout}: Props) => {
+const ProjectImageCard = ({ imageUrl, textDescription, layout }: Props) => {
   return (
-    <div className="flex flex-col gap-0 items-center justify-center">
-      <div className="w-[100%] h-[100%]  flex items-center justify-center">
+    <div className="relative flex items-center justify-center w-full h-auto overflow-hidden">
+      <div className="relative w-full h-full">
         <Image
           src={imageUrl}
           alt={"Project Image"}
           layout={layout}
           width={200}
           height={200}
-          className="object-contain  border-solid border-2 border-[#52525b]"
+          className="object-cover w-full h-full border-2 border-solid border-[#52525b]"
         />
-      </div>
-      <p className="font-nunito w-[90%] sm:w-[80%]">{textDescription}</p>
-    </div>
-  )
-}
 
-export default ProjectImageCard
+        <div className="absolute group inset-0 flex items-center justify-center hover:bg-black hover:bg-opacity-70">
+          <p className="font-nunito opacity-0 group-hover:opacity-100 text-[#F2F2F2] p-2 text-center">
+            {textDescription}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectImageCard;
