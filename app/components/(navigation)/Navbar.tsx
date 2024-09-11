94987823294d452e709e94d79e1b97bb410f57be
MartinPettersen <searchingtheskyfor@gmail.com>
2024-09-11 09:17:05 +0200

    fix bug where navigation did'nt work as intended

--- app/components/(navigation)/Navbar.tsx
+++ app/components/(navigation)/Navbar.tsx
@@ -1,36 +1,37 @@
 "use client";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const Navbar = () => {
-  const handleScrollTo = (element: string) => {
-    const section = document.getElementById(element);
-    if (section) {
-      section.scrollIntoView({ behavior: "smooth" });
-    }
+
+  const router = useRouter();
+
+  const handleNavigation = (path: string) => {
+    router.push(path); 
   };
   return (
     <div className="font-manrope flex flex-row gap-6 fixed right-10 top-0 pt-4 z-50 bg-[#F2F2F2] sm:bg-transparent">
       <h2
         className="hover:cursor-pointer"
-        onClick={() => handleScrollTo("home")}
+        onClick={() => handleNavigation('/#home')}
       >
         Home
       </h2>
       <h2
         className="hover:cursor-pointer"
-        onClick={() => handleScrollTo("about")}
+        onClick={() => handleNavigation('/#about')}
       >
         About
       </h2>
       <h2
         className="hover:cursor-pointer"
-        onClick={() => handleScrollTo("education")}
+        onClick={() => handleNavigation('/#education')}
       >
         Education
       </h2>
       <h2
         className="hover:cursor-pointer"
-        onClick={() => handleScrollTo("projects")}
+        onClick={() => handleNavigation('/#projects')}
       >
         Projects
       </h2>
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+import { useEffect } from "react";
 import Navbar from "./components/(navigation)/Navbar";
 import About from "./components/(pages)/About";
 import Education from "./components/(pages)/Education";
@@ -5,6 +7,17 @@ import Projects from "./components/(pages)/Projects";
 import Welcome from "./components/(pages)/Welcome";
 
 export default function Home() {
+
+  useEffect(() => {
+    const hash = window.location.hash.substring(1);
+    if(hash) {
+      const element = document.getElementById(hash);
+      if (element) {
+        element.scrollIntoView({ behavior: "smooth"});
+      }
+    }    
+  }, [])
+
   return (
     <div className="flex items-center justify-center flex-col min-h-screen p-8 pb-20  sm:p-20 ">
       <section id="home" className="snap-start">
